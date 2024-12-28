d1f4dad79ddf90dd8b56f0d7711d504deb3a9069
kushal3008 <131846101+kushal3008@users.noreply.github.com>
2024-12-28 14:11:21 +0530

    Register and Create a bill

--- mainmenu.py
+++ mainmenu.py
@@ -49,26 +49,18 @@ def mainMenu():
     checkButton = Button(text="Check",border=6,font=("Arial",10,"bold"),command=lambda :customers())
     checkButton.grid(row=6,column=0,sticky="w",padx=400)
 
-    # Clearing box after one product is added
 
-    def clear():
-
-        ProductBox.delete(0,tk.END)
-        QuantityBox.delete(0, tk.END)
-
-
-    add1 = Button(text="Add", width=20, border=6, command=clear,font=("Arial",10,"bold"))
+    add1 = Button(text="Add", width=20, border=6,font=("Arial",10,"bold"))
     add1.grid(padx=90, pady=10, row=11, column=0, sticky="w")
 
     def updateDatabase(event):
         global billAmount
         db = mysql.connector.connect(host="localhost", user="root", passwd="Kushal3008@", database="ShopLens")
         cursor = db.cursor()
-        saleProduct = str(ProductBox.get().lower())
+        saleProduct = str(ProductBox.get().lower().strip())
         saleQuantity = int(QuantityBox.get())
         salesDate = date.today()
-        query1 = "select Price from products where ProductName = %s;"
-        cursor.execute(query1,(saleProduct,))
+        cursor.execute(f"select Price from products where ProductName = '{saleProduct}';")
         result1 = cursor.fetchone()
         if result1:
             salesPrice = int(result1[0])
@@ -76,50 +68,53 @@ def mainMenu():
             salesPrice = 0
 
         salesAmount = saleQuantity * salesPrice
-        query3 = "select Quantity from Products where ProductName = %s;"
-        cursor.execute(query3,(saleProduct,))
+        cursor.execute(f"select Quantity from Products where ProductName = '{saleProduct}';")
         result2 = cursor.fetchone()
-        if result2:
+        if int(result2[0]) > 0:
             stockUpdate = int(result2[0]) - saleQuantity
+            billAmount += salesAmount
+            cursor.execute("select BillId from buys order by BillId desc limit 1;")
+            result3 = cursor.fetchone()
+            if result3:
+                billID = int(result3[0]) + 1
+            else:
+                billID = 1000
+            query2 = "insert into sales(BillId,Item,Quantity,PricePerUnit,Amount,Date) values (%s,%s,%s,%s,%s,%s)"
+            values = [billID, saleProduct, saleQuantity, salesPrice, salesAmount, salesDate]
+            cursor.execute(query2, values)
+            cursor.execute(f"update Products set Quantity = {stockUpdate} where ProductName = '{saleProduct}';")
+            db.commit()
+
+            # Displaying total amount of the purchase
+
+            totalAmount.config(state="normal")
+            totalAmount.delete(0, tk.END)  # Clear the entry before inserting new value
+            totalAmount.insert(tk.END, f"Total Amount:\t{billAmount}")
+            totalAmount.config(state="readonly")
+
+            # Displaying items added in cart
+
+            items = f"{saleProduct}\t\t\t\t\t\t\t{saleQuantity}\t\t\t\t\t\t\t{salesPrice}\t\t\t\t\t\t\t{salesAmount}"
+            billarea.config(state="normal")
+            billarea.insert(tk.END, items + "\n")
+            billarea.config(state="disabled")
+            ProductBox.delete(0, tk.END)
+            QuantityBox.delete(0, tk.END)
+            cursor.close()
+            db.close()
+        elif int(result2[0]) == 0:
+            messagebox.showinfo(message=f"!!{saleProduct.capitalize()} is out of stock!!")
+            ProductBox.delete(0, tk.END)
+            QuantityBox.delete(0, tk.END)
         else:
-            stockUpdate = 0
-
-        billAmount += salesAmount
-        cursor.execute("select BillId from buys order by BillId desc limit 1;")
-        result3 = cursor.fetchone()
-        if result3:
-            billID = int(result3[0]) + 1
-        else:
-            billID = 1000
-        query2 = "insert into sales(BillId,Item,Quantity,PricePerUnit,Amount,Date) values (%s,%s,%s,%s,%s,%s)"
-        values = [billID,saleProduct,saleQuantity,salesPrice,salesAmount,salesDate]
-        cursor.execute(query2,values)
-        cursor.execute(f"update Products set Quantity = {stockUpdate} where ProductName = '{saleProduct}';")
-        db.commit()
-
-        totalAmount.config(state="normal")
-        totalAmount.delete(0, tk.END)  # Clear the entry before inserting new value
-        totalAmount.insert(tk.END, f"Total Amount:\t{billAmount}")
-        totalAmount.config(state="readonly")
-
-
-
-        # Displaying items added in cart
-
-        items = f"{saleProduct}\t\t\t\t\t\t\t{saleQuantity}\t\t\t\t\t\t\t{salesPrice}\t\t\t\t\t\t\t{salesAmount}"
-        billarea.config(state="normal")
-        billarea.insert(tk.END, items + "\n")
-        billarea.config(state="disabled")
-        cursor.close()
-        db.close()
-
+            ProductBox.delete(0, tk.END)
+            QuantityBox.delete(0, tk.END)
 
     #Adding event to update database
 
     add1.bind("<Button-1>",updateDatabase)
 
     #Addinng Textarea where bill item will be displayed
-
 
     billarea = Text(height=20,width=186,background="light blue",font=('Arial',11,"bold"))
     billarea.config(state="normal")
@@ -180,17 +175,10 @@ def mainMenu():
         emailBox.delete(0,tk.END)
         customerName.delete(0,tk.END)
         billarea.config(state="normal")
-        billarea.delete(0,tk.END)
-        billarea.insert(tk.END, 297 * f"-" + "\n")
-        billarea.insert(tk.END,f"Product Name\t\t\t\t\t\t\tQuantity\t\t\t\t\t\t\tPrice Per Quantity\t\t\t\t\t\t\tAmount\n")
-        billarea.insert(tk.END, 297 * f"-" + "\n")
+        billarea.delete("4.0",tk.END)
         billarea.config(state="disabled")
         cursor.close()
         db.close()
-
-
-
-
 
     window.mainloop()
 
